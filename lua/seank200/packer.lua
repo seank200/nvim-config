df764ac4f4e234e6635e148cb0dca83ccb14f081
Youngwoo Kim <yw.sean.kim@gmail.com>
2023-12-14 20:54:50 +0900

    refactor/reduce requires, and shorter functions

--- lua/seank200/packer.lua
+++ lua/seank200/packer.lua
@@ -14,6 +14,17 @@ local packer_bootstrap = ensure_packer()
 -- Only required if you have packer configured as `opt`
 vim.cmd([[packadd packer.nvim]])
 
+local config = {
+	coding = require("seank200.plugins.coding"),
+	colorscheme = require("seank200.plugins.colorscheme"),
+	editor = require("seank200.plugins.editor"),
+	formatting = require("seank200.plugins.formatting"),
+	lsp = require("seank200.plugins.lsp"),
+	treesitter = require("seank200.plugins.treesitter"),
+	ui = require("seank200.plugins.ui"),
+	utils = require("seank200.plugins.utils"),
+}
+
 return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
@@ -22,32 +33,33 @@ return require("packer").startup(function(use)
 	use({
 		"echasnovski/mini.nvim",
 		branch = "stable",
-		config = require("seank200.plugins.coding").mini,
+		config = config.coding.mini,
 	})
 	use({
 		"JoosepAlviste/nvim-ts-context-commentstring",
-		config = require("seank200.plugins.coding").ts_context_commentstring,
+		config = config.coding.ts_context_commentstring,
 	})
 	use({
 		"mbbill/undotree",
-		config = require("seank200.plugins.coding").undo_tree,
+		config = config.coding.undo_tree,
 	})
+	use({ "markonm/traces.vim" })
 
 	-- ColorSchemes
 	use({
 		"catppuccin/nvim",
 		as = "catppuccin",
-		config = require("seank200.plugins.colorscheme").catppuccin,
+		config = config.colorscheme.catppuccin,
 	})
 
 	-- Editor
 	use({
 		"christoomey/vim-tmux-navigator",
-		config = require("seank200.plugins.editor").vim_tmux_navigator,
+		config = config.editor.vim_tmux_navigator,
 	})
 	use({
 		"mrjones2014/smart-splits.nvim",
-		config = require("seank200.plugins.editor").smart_splits,
+		config = config.editor.smart_splits,
 	})
 	use({
 		"nvim-telescope/telescope.nvim",
@@ -55,7 +67,7 @@ return require("packer").startup(function(use)
 		requires = {
 			{ "nvim-lua/plenary.nvim" },
 		},
-		config = require("seank200.plugins.editor").telescope,
+		config = config.editor.telescope,
 	})
 	use({
 		"nvim-telescope/telescope-file-browser.nvim",
@@ -64,7 +76,7 @@ return require("packer").startup(function(use)
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons",
 		},
-		config = require("seank200.plugins.editor").telescope_file_browser,
+		config = config.editor.telescope_file_browser,
 	})
 	use({
 		"nvim-telescope/telescope-fzf-native.nvim",
@@ -72,24 +84,24 @@ return require("packer").startup(function(use)
 		requires = {
 			"nvim-telescope/telescope.nvim",
 		},
-		config = require("seank200.plugins.editor").telescope_fzf,
+		config = config.editor.telescope_fzf,
 	})
 	use({ "nvim-pack/nvim-spectre" })
 	use({
 		"sindrets/diffview.nvim",
-		config = require("seank200.plugins.editor").diffview,
+		config = config.editor.diffview,
 	})
 
 	-- Formatting
 	use({
 		"stevearc/conform.nvim",
-		config = require("seank200.plugins.formatting").conform,
+		config = config.formatting.conform,
 	})
 
 	-- LSP
 	use({
 		"williamboman/mason.nvim",
-		config = require("seank200.plugins.lsp").mason,
+		config = config.lsp.mason,
 	})
 	use({
 		"williamboman/mason-lspconfig.nvim",
@@ -115,7 +127,7 @@ return require("packer").startup(function(use)
 			{ "hrsh7th/cmp-cmdline" },
 			{ "L3MON4D3/LuaSnip" },
 		},
-		config = require("seank200.plugins.lsp").lsp_zero,
+		config = config.lsp.lsp_zero,
 	})
 	use({
 		"folke/neodev.nvim",
@@ -128,7 +140,7 @@ return require("packer").startup(function(use)
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
-		config = require("seank200.plugins.treesitter").treesitter,
+		config = config.treesitter.treesitter,
 	})
 	use({
 		"nvim-treesitter/nvim-treesitter-textobjects",
@@ -139,7 +151,7 @@ return require("packer").startup(function(use)
 		"nvim-treesitter/nvim-treesitter-context",
 		after = "nvim-treesitter",
 		requires = "nvim-treesitter/nvim-treesitter",
-		config = require("seank200.plugins.treesitter").treesitter_context,
+		config = config.treesitter.treesitter_context,
 	})
 
 	-- UI
@@ -152,47 +164,50 @@ return require("packer").startup(function(use)
 			"MunifTanjim/nui.nvim",
 			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 		},
-		config = require("seank200.plugins.ui").neo_tree,
+		config = config.ui.neo_tree,
 	})
 	use({ "nvim-tree/nvim-web-devicons" })
-	use({ "lewis6991/gitsigns.nvim" })
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = config.ui.gitsigns,
+	})
 	use({
 		"RRethy/vim-illuminate",
-		config = require("seank200.plugins.ui").illuminate,
+		config = config.ui.illuminate,
 	})
 	use({
 		"folke/trouble.nvim",
 		requires = "nvim-tree/nvim-web-devicons",
-		config = require("seank200.plugins.ui").trouble,
+		config = config.ui.trouble,
 	})
 	use({
 		"folke/todo-comments.nvim",
 		requires = { "nvim-lua/plenary.nvim" },
-		config = require("seank200.plugins.ui").todo_comments,
+		config = config.ui.todo_comments,
 	})
 	use({
 		"akinsho/bufferline.nvim",
 		tag = "*",
 		requires = "nvim-tree/nvim-web-devicons",
-		config = require("seank200.plugins.ui").bufferline,
+		config = config.ui.bufferline,
 	})
 	use({
 		"nvim-lualine/lualine.nvim",
 		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-		config = require("seank200.plugins.ui").lualine,
+		config = config.ui.lualine,
 	})
 	use({
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",
 		requires = { "nvim-tree/nvim-web-devicons" },
-		config = require("seank200.plugins.ui").dashboard,
+		config = config.ui.dashboard,
 	})
 
 	-- Utils
 	use({ "nvim-lua/plenary.nvim" })
 	use({
 		"folke/persistence.nvim",
-		config = require("seank200.plugins.utils").persistence,
+		config = config.utils.persistence,
 	})
 
 	-- Automatically install and configure if packer was first installed
--- lua/seank200/plugins/coding.lua
+++ lua/seank200/plugins/coding.lua
@@ -1,54 +1,54 @@
 local config = {}
 
 config.mini = function()
-    require("mini.bufremove").setup()
+	require("mini.bufremove").setup()
 
-    --- Delete the current buffer using mini.bufremove
-    ---
-    ---@param force boolean | nil
-    ---
-    ---@return nil
-    local function delete_buf(force)
-        MiniBufremove.delete(vim.api.nvim_get_current_buf(), force)
-    end
+	--- Delete the current buffer using mini.bufremove
+	local function delete_buf()
+		MiniBufremove.delete(vim.api.nvim_get_current_buf())
+	end
 
-    vim.keymap.set('n', '<leader>bd', function() delete_buf(false) end, { noremap = true })
-    vim.keymap.set('n', '<leader>bD', function() delete_buf(true) end, { noremap = true })
+	local function delete_buf_force()
+		MiniBufremove.delete(vim.api.nvim_get_current_buf(), true)
+	end
 
-    require("mini.comment").setup({
-        options = {
-            custom_commentstring = function()
-                return require('ts_context_commentstring').calculate_commentstring() or vim.bo.commentstring
-            end,
-        },
-    })
+	vim.keymap.set("n", "<leader>bd", delete_buf, { noremap = true })
+	vim.keymap.set("n", "<leader>bD", delete_buf_force, { noremap = true })
 
-    require("mini.pairs").setup()
+	require("mini.comment").setup({
+		options = {
+			custom_commentstring = function()
+				return require("ts_context_commentstring").calculate_commentstring() or vim.bo.commentstring
+			end,
+		},
+	})
 
-    require("mini.surround").setup({
-        mappings = {
-            add = 'gsa',
-            delete = 'gsd',
-            find = 'gsf',
-            find_left = 'gsF',
-            highlight = 'gsh',
-            replace = 'gsr',
-            update_n_lines = 'gsn',
-        }
-    })
+	require("mini.pairs").setup()
+
+	require("mini.surround").setup({
+		mappings = {
+			add = "gsa",
+			delete = "gsd",
+			find = "gsf",
+			find_left = "gsF",
+			highlight = "gsh",
+			replace = "gsr",
+			update_n_lines = "gsn",
+		},
+	})
 end
 
 config.ts_context_commentstring = function()
-    require('ts_context_commentstring').setup({
-        enable_autocmd = false,
-    })
+	require("ts_context_commentstring").setup({
+		enable_autocmd = false,
+	})
 end
 
-config.undo_tree = function ()
-    vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+config.undo_tree = function()
+	vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
-    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-    vim.opt.undofile = true
+	vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+	vim.opt.undofile = true
 end
 
 return config
